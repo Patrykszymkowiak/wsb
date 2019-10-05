369d298b04e366aa9457b0cfc6c0d07f4c888bc3
Patrykszymkowiak <priceq9@gmail.com>
2019-10-05 11:28:55 +0200

    Pierwsze zajęcia

---
+++ podstawy_programowania/1_firstProject/1_firstProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_firstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Imie: ");
+            Console.WriteLine("Janusz");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("#\n##\n###");
+            string name = "Patryk";
+            Console.WriteLine("Masz na imię " + name); // Konkatynancja +
+            Console.WriteLine("Masz na imię {0}",name);
+            int age = 23;
+            Console.WriteLine("Masz na imię {0}, twój wiek {1}",name,age);
+            sbyte age1 = 23;
+            Console.WriteLine("Masz na imię {0}, twój wiek1 {1}", name, age1);
+            Console.ReadKey();
+        }
+    }
+}
